32c04a50e3adee474328b9b176d1dfe3c077efd6
Deep1k07 <deepak.tnv@gmail.com>
2026-04-01 15:42:34 +0530

    add swegger docs

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
@@ -19,6 +20,16 @@ async function bootstrap() {
       forbidNonWhitelisted: true, // throws error for extra fields
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('MIS API')
+    .setDescription('MIS Backend API Documentation')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(process.env.PORT ?? 3003);
 }
 bootstrap();
