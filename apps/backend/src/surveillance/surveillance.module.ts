175b7e175e9618bb4eb77ef1ee8f739a5830fb37
Deep1k07 <deepak.tnv@gmail.com>
2026-04-16 12:05:02 +0530

    feat: implement surveillance module with controller, service, and mongoose schema integration

--- apps/backend/src/surveillance/surveillance.module.ts
+++ apps/backend/src/surveillance/surveillance.module.ts
@@ -1,4 +1,18 @@
 import { Module } from '@nestjs/common';
+import { SurveillanceController } from './surveillance.controller';
+import { SurveillanceService } from './surveillance.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { SurveillanceOne, SurveillanceOneSchema } from './schema/surveillanceOne.schema';
+import { SurveillanceTwo, SurveillanceTwoSchema } from './schema/surveillanceTwo.schema';
 
-@Module({})
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: SurveillanceOne.name, schema: SurveillanceOneSchema },
+      { name: SurveillanceTwo.name, schema: SurveillanceTwoSchema },
+    ]),
+  ],
+  controllers: [SurveillanceController],
+  providers: [SurveillanceService]
+})
 export class SurveillanceModule {}
